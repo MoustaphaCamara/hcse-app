47be470c66fc5d9c86d60ab445ea196724a9f326
Mouss <104678778+MoussCarter@users.noreply.github.com>
2023-04-07 21:34:54 +0200

    li responsive

--- resources/js/components/star/StarList.vue
+++ resources/js/components/star/StarList.vue
@@ -2,7 +2,7 @@
   <ul
     class="sm:w-1/3 w-full bg-dark-second border-dark-second text-slate-800 flex flex-col items-center"
   >
-    <li>
+    <li class="w-full">
       <button
         class="bg-second relative inline-flex items-center w-full px-4 py-2 text-sm font-medium border-b text-slate-900 border-dark-second hover:bg-dark-second active:bg-white focus:bg-light-main"
         v-for="(star, key) in stars"
